2bfffe9cc62797b7b1be74229bf0db0bc7f64a4b
saumya <2saumyaray@gmail.com>
2016-03-09 13:07:00 +0530

    Cards play

--- js/components/controllerViewHome.react.js
+++ js/components/controllerViewHome.react.js
@@ -1,6 +1,12 @@
 //
 var React = require('react');
+//
 var RaisedButton = require('material-ui/lib/raised-button');
+var FlatButton = require('material-ui/lib/flat-button');
+var Card = require('material-ui/lib/card/card');
+var CardActions = require('material-ui/lib/card/card-actions');
+var CardHeader = require('material-ui/lib/card/card-header');
+var CardText = require('material-ui/lib/card/card-text');
 //
 var HomeControllerView = React.createClass({
   render: function() {
@@ -9,10 +15,28 @@ var HomeControllerView = React.createClass({
     }
     //
     return (
-      <div>
-        Welcome to your immediate requirement.
-        <RaisedButton label="Material Button !" onTouchTap={onTouchTap}  />
-      </div>
+      <Card>
+        <CardHeader
+          title="Without Avatar"
+          subtitle="Subtitle"
+          actAsExpander={true}
+          showExpandableButton={true}
+        />
+        <CardText expandable={true}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
+          Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
+          Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
+          <div>
+            Welcome to your immediate requirement.
+            <RaisedButton label="Material Button !" onTouchTap={onTouchTap}  />
+          </div>
+        </CardText>
+        <CardActions expandable={true}>
+          <FlatButton label="Action1" />
+          <FlatButton label="Action2" />
+        </CardActions>
+      </Card>
     );
   }
 });
