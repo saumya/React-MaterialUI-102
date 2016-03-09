59ac6eafcac544092fe7b2a63eedc7d9967ddcd0
saumya <2saumyaray@gmail.com>
2016-03-09 14:05:40 +0530

    List is added

--- js/components/controllerViewSeeAll.react.js
+++ js/components/controllerViewSeeAll.react.js
@@ -1,12 +1,37 @@
 //
 var React = require('react');
+//
+//var MobileTearSheet = require('../../../MobileTearSheet');
+var SelectableContainerEnhance = require('material-ui/lib/hoc/selectable-enhance');
+
+var List = require('material-ui/lib/lists/list');
+var ListItem = require('material-ui/lib/lists/list-item');
+var ActionGrade = require('material-ui/lib/svg-icons/action/grade');
+var ActionInfo = require('material-ui/lib/svg-icons/action/info');
+var ContentInbox = require('material-ui/lib/svg-icons/content/inbox');
+var ContentDrafts = require('material-ui/lib/svg-icons/content/drafts');
+var ContentSend = require('material-ui/lib/svg-icons/content/send');
+var Divider = require('material-ui/lib/divider');
+
 
 var SeeAllControllerView = React.createClass({
   render: function(){
     return (
       <div>
-        See All Saved
-
+          <List>
+            <ListItem primaryText="Inbox" leftIcon={<ContentInbox />} />
+            <ListItem primaryText="Starred" leftIcon={<ActionGrade />} />
+            <ListItem primaryText="Sent mail" leftIcon={<ContentSend />} />
+            <ListItem primaryText="Drafts" leftIcon={<ContentDrafts />} />
+            <ListItem primaryText="Inbox" leftIcon={<ContentInbox />} />
+          </List>
+        <Divider />
+          <List>
+            <ListItem primaryText="All mail" rightIcon={<ActionInfo />} />
+            <ListItem primaryText="Trash" rightIcon={<ActionInfo />} />
+            <ListItem primaryText="Spam" rightIcon={<ActionInfo />} />
+            <ListItem primaryText="Follow up" rightIcon={<ActionInfo />} />
+          </List>
       </div>
     );
   }
